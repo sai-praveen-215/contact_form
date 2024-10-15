5d45df72d0b4b5a61f22b4d89481aeb7b35a323b
sai-praveen-215 <saipraveeneps@gmail.com>
2024-10-15 18:21:52 +0530

    tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,42 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+import { Col, Form, Input, Row } from "antd";
+import "./Tailwind.css";
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="bg-green-200 h-screen w-screen align-middle justify-center flex ">
+      <div className="w-[50%] h-[80%] bg-white mt-20 shadow-2xl p-6">
+        <Form layout="vertical">
+          <h1 className="text-4xl font-bold text-cyan-700">Contact Us</h1>
+          <Row gutter={[16, 16]}>
+            <Col xl={12} xxl={12} sm={12} md={12} xs={24}>
+              <Form.Item name="first_name" label="First Name">
+                <Input placeholder="Plese Enter First Name" />
+              </Form.Item>
+            </Col>
+            <Col xl={12} xxl={12} sm={12} md={12} xs={24}>
+              <Form.Item name="last_name" label="Last Name">
+                <Input placeholder="Plese Enter Last Name" />
+              </Form.Item>
+            </Col>
+          </Row>
+          <Row>
+            <Col xl={24} xxl={24} sm={24} md={24} xs={24}>
+              <Form.Item name="email" label="Email">
+                <Input placeholder="Plese Enter Email" />
+              </Form.Item>
+            </Col>
+          </Row>
+          <Row>
+            <Col xl={12} xxl={12} sm={12} md={12} xs={24}>
+              <Form.Item name="General Enqury">
+                <label></label>
+                General Enqury <Input type="radio" />
+              </Form.Item>
+            </Col>
+          </Row>
+        </Form>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+// tailwind.config.mjs
+export default {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
